6f71461ca95a375a5a866672681648386b4a1c62
ZxStim <thang.victor.luong@gmail.com>
2025-04-17 11:14:53 +0700

    fix: devforum stats

--- app/dashboard/devforum/page.tsx
+++ app/dashboard/devforum/page.tsx
@@ -1,5 +1,5 @@
 import devForum from "@/lib/mocks/kaia-devforum.json";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, Package, UserPen, Users } from "lucide-react";
 
 type DevForumPost = {
   id: number;
@@ -7,13 +7,42 @@ type DevForumPost = {
   url: string;
 }
 
+const stats = {
+  totalPosts: 3492,
+  totalMembers: 1970,
+  totalAdminsMods: 13,
+}
+
 export default function DevForumPage() {
   return (
     <div className="flex flex-col gap-4 p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h1 className="text-2xl font-bold">{stats.totalPosts}</h1>
+          <div className="flex items-center gap-2">
+            <Package className="w-4 h-4" />
+            <p className="text-sm">Total posts</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h1 className="text-2xl font-bold">{stats.totalMembers}</h1>
+          <div className="flex items-center gap-2">
+            <UserPen className="w-4 h-4" />
+            <p className="text-sm">Total members</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h1 className="text-2xl font-bold">{stats.totalAdminsMods}</h1>
+          <div className="flex items-center gap-2">
+            <Users className="w-4 h-4" />
+            <p className="text-sm">Total admins + mods</p>
+          </div>
+        </div>
+      </div>
       <h1 className="text-2xl font-bold">Latest Posts</h1>
       <div className="flex flex-col gap-4">
         {devForum.devforum.map((post: DevForumPost) => (
-          <div key={post.id} className="flex flex-col gap-2">
+          <div key={post.id} className="flex flex-col gap-2 w-fit">
             <a href={post.url} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 underline underline-offset-4">
               <h2 className="text-lg">{post.title}</h2>
               <ExternalLink className="w-4 h-4" />
--- app/dashboard/discord/page.tsx
+++ app/dashboard/discord/page.tsx
@@ -1,3 +1,38 @@
+import { Package, UserPen, Users } from "lucide-react";
+
 export default function DiscordPage() {
-  return <div>Discord</div>;
+  const stats = {
+    totalRepositories: 0,
+    totalAuthors: 0,
+    totalContributors: 0,
+  }
+
+  
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h1 className="text-2xl font-bold">{stats.totalRepositories}</h1>
+          <div className="flex items-center gap-2">
+            <Package className="w-4 h-4" />
+            <p className="text-sm">Repositories</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h1 className="text-2xl font-bold">{stats.totalAuthors}</h1>
+          <div className="flex items-center gap-2">
+            <UserPen className="w-4 h-4" />
+            <p className="text-sm">Authors</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h1 className="text-2xl font-bold">{stats.totalContributors}</h1>
+          <div className="flex items-center gap-2">
+            <Users className="w-4 h-4" />
+            <p className="text-sm">Contributors</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
