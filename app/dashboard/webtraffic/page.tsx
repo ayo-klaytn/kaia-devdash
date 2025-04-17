a737f4ded8535f04d5a41971cd9991aa04a84cca
ZxStim <thang.victor.luong@gmail.com>
2025-04-17 11:58:53 +0700

    feat: added web data

--- app/dashboard/webtraffic/page.tsx
+++ app/dashboard/webtraffic/page.tsx
@@ -2,8 +2,14 @@ import { UserPen, Package, Users } from "lucide-react";
 import webTrafficData from "@/lib/mocks/kaia-docs-webtraffic.json";
 import { WebTrafficChart } from "@/app/dashboard/webtraffic/chart";
 export default function WebTrafficPage() {
-
-  const { overview, daily_stats, pages, referrers, browsers, operating_systems, devices } = webTrafficData;
+  const {
+    overview,
+    pages,
+    referrers,
+    browsers,
+    operating_systems,
+    devices,
+  } = webTrafficData;
 
   return (
     <div className="flex flex-col gap-4 p-4">
@@ -37,7 +43,9 @@ export default function WebTrafficPage() {
           </div>
         </div>
         <div className="flex flex-col gap-4 border rounded-md p-4">
-          <h1 className="text-2xl font-bold">{overview.visit_duration.value}</h1>
+          <h1 className="text-2xl font-bold">
+            {overview.visit_duration.value}
+          </h1>
           <div className="flex items-center gap-2">
             <Users className="w-4 h-4" />
             <p className="text-sm">Visit Duration</p>
@@ -47,6 +55,70 @@ export default function WebTrafficPage() {
       <div className="flex flex-col gap-4">
         <WebTrafficChart />
       </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h2 className="text-lg font-bold">Pages</h2>
+          {pages.map((page) => (
+            <div
+              key={page.path}
+              className="flex flex-row gap-4 justify-between"
+            >
+              <h3 className="text-sm">{page.path}</h3>
+              <p className="text-sm">{page.views}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h2 className="text-lg font-bold">Referrers</h2>
+          {referrers.map((referrer) => (
+            <div
+              key={referrer.source}
+              className="flex flex-row gap-4 justify-between"
+            >
+              <h3 className="text-sm">{referrer.source}</h3>
+              <p className="text-sm">{referrer.visitors}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h2 className="text-lg font-bold">Browsers</h2>
+          {browsers.map((browser) => (
+            <div
+              key={browser.name}
+              className="flex flex-row gap-4 justify-between"
+            >
+              <h3 className="text-sm">{browser.name}</h3>
+              <p className="text-sm">{browser.visitors}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h2 className="text-lg font-bold">Operating Systems</h2>
+          {operating_systems.map((operating_system) => (
+            <div
+              key={operating_system.name}
+              className="flex flex-row gap-4 justify-between"
+            >
+              <h3 className="text-sm">{operating_system.name}</h3>
+              <p className="text-sm">{operating_system.visitors}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4 border rounded-md p-4">
+          <h2 className="text-lg font-bold">Devices</h2>
+          {devices.map((device) => (
+            <div
+              key={device.type}
+              className="flex flex-row gap-4 justify-between"
+            >
+              <h3 className="text-sm">{device.type}</h3>
+              <p className="text-sm">{device.visitors}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
