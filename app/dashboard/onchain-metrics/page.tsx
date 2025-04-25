9de8c49779c9b2b53741f2042c55e5079278428d
ZxStim <thang.victor.luong@gmail.com>
2025-04-25 12:46:01 +0700

    feat: added onchain metrics

--- app/dashboard/onchain-metrics/page.tsx
+++ app/dashboard/onchain-metrics/page.tsx
@@ -30,7 +30,40 @@ export default async function OnchainMetricsPage() {
           </Button>
         </div>
       </div>
-      <div className="flex flex-row flex-wrap gap-4">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-bold">
+            Number of active contracts
+          </p>
+          <iframe src="https://dune.com/embeds/4216780/7101037" className="w-full h-[400px] border-1 border-orange-500"/>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-bold">
+            Hot contracts 🔥
+          </p>
+          <iframe src="https://dune.com/embeds/4220772/7101058" className="w-full h-[600px] border-1 border-orange-500"/>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-bold">
+            TVL (source: Defillama)
+          </p>
+          <iframe src="https://dune.com/embeds/4222136/7103243" className="w-full h-[400px] border-1 border-orange-500"/>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col gap-4">
+            <p className="text-lg font-bold">
+              Max TPS
+            </p>
+            <iframe src="https://dune.com/embeds/4674606/7780357" className="w-full h-[400px] border-1 border-orange-500"/>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="text-lg font-bold">
+              Daily TPS
+            </p>
+            <iframe src="https://dune.com/embeds/4225892/7109149" className="w-full h-[400px] border-1 border-orange-500"/>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
