bb7013743510c27bcb161764928d09d0f5c2b91a
ZxStim <thang.victor.luong@gmail.com>
2025-04-25 13:07:21 +0700

    fix: disable stream for nginx

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'X-Accel-Buffering',
+            value: 'no',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
