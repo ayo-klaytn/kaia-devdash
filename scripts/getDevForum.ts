6311ff654b7e492354c62c253f17573cf929376c
ZxStim <thang.victor.luong@gmail.com>
2025-05-12 13:51:11 +0700

    feat: added discourse client

---
+++ scripts/getDevForum.ts
@@ -0,0 +1,35 @@
+class DiscourseClient {
+  private baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getCategories() {
+    const response = await fetch(`${this.baseUrl}/categories.json`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  async getLatestPosts() {
+    const response = await fetch(`${this.baseUrl}/posts.json`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  }
+}
+
+async function main() {
+  const client = new DiscourseClient("https://devforum.kaia.io");
+  const latestPosts = await client.getLatestPosts();
+  console.log("Latest posts:", latestPosts);
+}
+
+main();
