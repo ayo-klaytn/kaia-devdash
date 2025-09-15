79c0a212cb89352c5c7505aa9975b201f9e44add
ayo-klaytn <peter.ayo@klaytn.foundation>
2025-09-15 16:54:31 +0100

    updated

--- middleware.ts
+++ middleware.ts
@@ -5,17 +5,19 @@ import { NextRequest, NextResponse } from "next/server";
 type Session = typeof auth.$Infer.Session;
  
 export async function middleware(request: NextRequest) {
-	const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
-		baseURL: request.nextUrl.origin,
-		headers: {
-			cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
-		},
-	});
- 
-	if (!session) {
-		return NextResponse.redirect(new URL("/signin", request.url));
-	}
- 
+	// Temporarily disable authentication for Vercel testing
+	// TODO: Re-enable authentication once we debug the session issues
+	// const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
+	// 	baseURL: request.nextUrl.origin,
+	// 	headers: {
+	// 		cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+	// 	},
+	// });
+
+	// if (!session) {
+	// 	return NextResponse.redirect(new URL("/signin", request.url));
+	// }
+
 	return NextResponse.next();
 }
  
